d28e68a16f37f96477d49cedfddb63ff9298591d
Sushman <sascha.ecks@web.de>
2023-02-04 00:50:54 +0100

    Added (not ready) function that calculates the coordinate range of currently visible tiles

--- Tree_sim/Scripts/World.gd
+++ Tree_sim/Scripts/World.gd
@@ -8,11 +8,15 @@ export var tile_size = 16
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var player = get_node("Player")
 	player.position = Vector2(player.pos_x * tile_size, player.pos_y * tile_size)
-	
+	get_node("Camera2D/Label").set_text("upper left:" + str(get_visible_rect().position) + "\n" + "lower right: " + str(get_visible_rect().end))
+
+
+func get_visible_rect():
+	return Rect2((get_node("Camera2D").position - get_viewport_rect().size / 2) , get_viewport_rect().size)
