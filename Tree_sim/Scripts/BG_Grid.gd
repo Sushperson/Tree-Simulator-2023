a198f9face5b59c3e8e1556885767172063cf8b6
Sushman <sascha.ecks@web.de>
2023-02-04 12:21:04 +0100

    Added Tilemap for Background

---
+++ Tree_sim/Scripts/BG_Grid.gd
@@ -0,0 +1,5 @@
+extends TileMap
+
+func generate_tile(x, y):
+	pass #if(x == 0):
+		
--- Tree_sim/Scripts/World.gd
+++ Tree_sim/Scripts/World.gd
@@ -2,16 +2,18 @@ extends Node2D
 
 
 # Declare member variables here. Examples:
-export var tile_size = 32
-export var tick_length = 1
+export var tile_size = 64
+export var tick_length = 1.0
 # var b = "text"
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("TileMap").cell_size.x = tile_size
-	get_node("TileMap").cell_size.x = tile_size
+	get_node("RootGrid").cell_size.x = tile_size
+	get_node("RootGrid").cell_size.y = tile_size
+	get_node("BG_Grid").cell_size.x = tile_size
+	get_node("BG_Grid").cell_size.y = tile_size
 	get_node("Tick_clock").connect("timeout", self, "tick")
 	get_node("Tick_clock").start(tick_length)
 
@@ -28,14 +30,16 @@ func tick():
 	player.position = Vector2(player.pos_x * tile_size + tile_size/2, player.pos_y * tile_size + tile_size/2)
 	set_player_tile()
 	
+	print(get_node("Camera2D").position)
 	get_node("Camera2D/Label").set_text("upper left:" + str(get_visible_rect().position) + "\n" + "lower right: " + str(get_visible_rect().end))
 	
 func set_player_tile():
 	var player = get_node("Player")
-	var tilemap = get_node("TileMap")
+	var tilemap = get_node("RootGrid")
 	tilemap.set_cell(player.get_last_x(), player.get_last_y(), player.last_tile)
 
 
 
 func get_visible_rect():
-	return Rect2((get_node("Camera2D").position - get_viewport_rect().size / 2) , get_viewport_rect().size)
+	var camera = get_node("Camera2D")
+	return Rect2((camera.position - (get_viewport_rect().size / 2 * camera.zoom)) / tile_size , get_viewport_rect().size * camera.zoom / tile_size)
